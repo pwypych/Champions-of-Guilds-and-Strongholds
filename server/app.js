1d5e794bc9dc8384830a77f8154195f3afa664a3
Piotrek <ludzik3217@gmail.com>
2018-09-13 00:28:29 +0200

    Add .prettierrc file

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
-const express = require('express');
+// @format
+
+const express = require("express");
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-})
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
 
 app.listen(3000);
