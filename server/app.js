f6ccde81067f19ccf96b62f7c320dec67b536f78
Piotrek <ludzik3217@gmail.com>
2018-09-15 00:11:33 +0200

    Add ws dependency, make basic web socket connection

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 // @format
 
 const express = require('express');
+const ws = require('ws');
 const app = express();
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
@@ -9,3 +11,14 @@ app.get('/', function(req, res) {
 });
 
 app.listen(3000);
+
+var WebSocketServer = require('ws').Server,
+  wss = new WebSocketServer({ port: 40510 });
+
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    console.log('received: %s', message);
+  });
+
+  setInterval(() => ws.send(`${new Date()}`), 1000);
+});
