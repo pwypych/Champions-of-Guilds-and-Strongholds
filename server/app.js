18c65ee230f66f9b89bd2e590e9e7bd494f19498
Piotrek <ludzik3217@gmail.com>
2018-09-14 23:49:18 +0200

    Force prettier to use single quotes in .prettierrc

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 // @format
 
-const express = require("express");
+const express = require('express');
 const app = express();
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", function(req, res) {
-  res.send("Hello World");
+app.get('/', function(req, res) {
+  res.send('Hello World');
 });
 
 app.listen(3000);
