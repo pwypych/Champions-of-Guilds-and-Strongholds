41fc3104beb40eeaa24f6e3e1a32b2ca6a3f02ea
Matteus <post@kalis.no>
2018-10-01 18:03:49 +0200

    Add no-underscore-dangle to .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'no-use-before-define': ['error', {functions: false}], // Can use hoisting of functions
     'no-param-reassign': ['error', {props: false}], // Do not warn for reassigning properties (must be for express, example request.session)
     'prefer-destructuring': 0, // Will not throw errors on syntax like: var something = request.body.something;
-    // 'no-underscore-dangle': ['error', {allow: ['_id']}], // Allow dangle for _id mongodb property
+    'no-underscore-dangle': ['error', {allow: ['_id']}], // Allow dangle for _id mongodb property
     // 'object-shorthand': ['error', 'consistent'], // Can use { variable: variable } in objects
     'prefer-template': 0,
     'arrow-body-style': ['error', 'always'] // Always braces when using arrow functions
