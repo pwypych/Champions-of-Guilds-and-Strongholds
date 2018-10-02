6d2b48a9ef53d3a682f5a7d3359c82897e15c95a
Matteus <post@kalis.no>
2018-10-02 19:48:14 +0200

    Add object-shorthand rule to .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     'no-param-reassign': ['error', {props: false}], // Do not warn for reassigning properties (must be for express, example request.session)
     'prefer-destructuring': 0, // Will not throw errors on syntax like: var something = request.body.something;
     'no-underscore-dangle': ['error', {allow: ['_id']}], // Allow dangle for _id mongodb property
-    // 'object-shorthand': ['error', 'consistent'], // Can use { variable: variable } in objects
+    'object-shorthand': ['error', 'consistent'], // Can use { variable: variable } in objects
     'prefer-template': 0,
     'arrow-body-style': ['error', 'always'] // Always braces when using arrow functions
   }
