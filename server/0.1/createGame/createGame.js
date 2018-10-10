aaa4c6cf06ac4a96af6a569a6294718562d3285c
Piotrek <ludzik3217@gmail.com>
2018-10-10 19:14:30 +0200

    Add findGameInstanceArray() to createGame.js

--- server/0.1/createGame/createGame.js
+++ server/0.1/createGame/createGame.js
@@ -37,6 +37,32 @@ module.exports = (environment, db, templateToHtml) => {
           viewModel.mapNameArray = mapNameArray;
 
           debug('findMapNameArray', mapNameArray);
+          findActiveGames();
+        });
+    }
+
+    function findActiveGames() {
+      const query = {};
+      const options = {};
+      options.projection = { mapLayer: 0 };
+
+      db.collection('games')
+        .find(query, options)
+        .toArray((error, gameInstanceArray) => {
+          if (error) {
+            debug('findActiveGames: error:', error);
+            res
+              .status(503)
+              .send(
+                '503 - Error - Cannot find activ games in games collection'
+              );
+          }
+
+          const gameInstances = gameInstanceArray;
+
+          viewModel.gameInstances = gameInstances;
+
+          debug('findMapNameArray', gameInstances);
           sendResponce();
         });
     }
