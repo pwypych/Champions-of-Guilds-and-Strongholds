66a3664be9b691fe95826db9ac550d07ac56731e
Piotrek <ludzik3217@gmail.com>
2018-10-17 19:53:49 +0200

    Add first part tutorial cod of pixijs to pixiTutorial.js

--- client/pixiTutorial.js
+++ client/pixiTutorial.js
@@ -2,11 +2,11 @@
 
 /* pixiTutorial.js */
 
-let type = 'WebGL';
-if (!PIXI.utils.isWebGLSupported()) {
-  type = 'canvas';
-}
-
-PIXI.utils.sayHello(type);
+let options = {};
+options.width = 256;
+options.height = 256;
+options.backgroundColor = '0xff0000';
+let app = new PIXI.Application(options);
+document.body.appendChild(app.view);
 
 /* /pixiTutorial.js */
