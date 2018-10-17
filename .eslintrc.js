7fd0e3ba9aa406e58755f9fd2fdb031bb6f1901a
Matteus <post@kalis.no>
2018-10-17 20:13:39 +0200

    Add basic structure of client side modules, and config eslint accordingly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,26 @@
 // @format
 
 module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'script'
   },
   extends: ['airbnb-base', 'prettier'],
-  globals: {},
+  globals: {
+    _: false,
+    $: false,
+    PIXI: false,
+    moduleArray: false
+  },
   rules: {
-    'no-use-before-define': ['error', {functions: false}], // Can use hoisting of functions
-    'no-param-reassign': ['error', {props: false}], // Do not warn for reassigning properties (must be for express, example request.session)
+    'no-use-before-define': ['error', { functions: false }], // Can use hoisting of functions
+    'no-param-reassign': ['error', { props: false }], // Do not warn for reassigning properties (must be for express, example request.session)
     'prefer-destructuring': 0, // Will not throw errors on syntax like: var something = request.body.something;
-    'no-underscore-dangle': ['error', {allow: ['_id']}], // Allow dangle for _id mongodb property
+    'no-underscore-dangle': ['error', { allow: ['_id'] }], // Allow dangle for _id mongodb property
     'object-shorthand': ['error', 'consistent'], // Can use { variable: variable } in objects
     'prefer-template': 0,
     'arrow-body-style': ['error', 'always'] // Always braces when using arrow functions
--- client/pixiTutorial.js
+++ client/pixiTutorial.js
@@ -1,12 +1,14 @@
 'use strict';
 
 /* pixiTutorial.js */
+(function init(document, $, _, pixi, moduleArray) {
 
-let options = {};
-options.width = 256;
-options.height = 256;
-options.backgroundColor = '0xff0000';
-let app = new PIXI.Application(options);
-document.body.appendChild(app.view);
+  const options = {};
+  options.width = 256;
+  options.height = 256;
+  options.backgroundColor = '0xff0000';
+  const app = new pixi.Application(options);
+  document.body.appendChild(app.view);
 
+})(document, $, _, PIXI, moduleArray);
 /* /pixiTutorial.js */
