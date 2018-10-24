7b4d35bcc7df69c478e35a6d1c5e0d297fe2f741
Piotrek <ludzik3217@gmail.com>
2018-10-24 19:40:08 +0200

    Create basic structure in readMap.js

--- server/api/readMap.js
+++ server/api/readMap.js
@@ -1,1 +1,43 @@
-// readMAp
+// @format
+
+'use strict';
+
+const debug = require('debug')('cogs:readMap');
+
+module.exports = (environment, db) => {
+  return (req, res) => {
+    const viewModel = {};
+    viewModel.baseurl = environment.baseurl;
+    viewModel.timestamp = Date.now();
+
+    (function init() {
+      debug('init');
+      checkRequestQuery();
+    })();
+
+    function checkRequestQuery() {
+      debug('req.query - ', req.query);
+      res.send('api/readMap');
+    }
+
+    function findMap(mapName) {
+      const query = { _id: mapName };
+      const options = {};
+
+      db.collection('mapCollection').findOne(
+        query,
+        options,
+        (error, mapObject) => {
+          if (error) {
+            debug('findMap: error:', error);
+            res.status(503).send('503 Error - Cannot find map');
+            return;
+          }
+
+          debug('findMap', mapObject._id);
+          prepareGameInstance(mapObject);
+        }
+      );
+    }
+  };
+};
