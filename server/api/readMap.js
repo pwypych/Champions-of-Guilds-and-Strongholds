ab1d13a7680ce316dd4bcae1f5c2f265c8fed683
Piotrek <ludzik3217@gmail.com>
2018-10-29 19:02:59 +0100

    Remove validations of gameInstanceId and playerToken from readMap.js

--- server/api/readMap.js
+++ server/api/readMap.js
@@ -4,103 +4,15 @@
 
 const debug = require('debug')('cogs:readMap');
 
-module.exports = (sanitizer, db) => {
+module.exports = (db) => {
   return (req, res) => {
-    let gameInstanceId;
-    let playerToken;
-
     (function init() {
       debug('init');
-      checkRequestQuery();
+      findMapLayer();
     })();
 
-    function checkRequestQuery() {
-      debug('req.query', req.query);
-      if (
-        typeof req.query.gameInstanceId === 'undefined' ||
-        typeof req.query.playerToken === 'undefined'
-      ) {
-        res.status(403).send('403 Forbbidden - Missing querry variable');
-        return;
-      }
-
-      gameInstanceId = req.query.gameInstanceId;
-      playerToken = req.query.playerToken;
-
-      debug('checkRequestQuery:', playerToken, gameInstanceId);
-      validateRequestQuery();
-    }
-
-    function validateRequestQuery() {
-      if (!sanitizer.isValidShortId(gameInstanceId)) {
-        debug('validateRequestQuery: Invalid gameInstanceId:', gameInstanceId);
-        res
-          .status(503)
-          .send('503 Service Unavailable - Invalid gameInstanceId');
-        return;
-      }
-
-      if (!sanitizer.isValidShortId(playerToken)) {
-        debug('validateRequestQuery: invalid playerToken:', playerToken);
-        res.status(503).send('503 Service Unavailable - Invalid playerToken');
-        return;
-      }
-
-      debug('validateRequestQuery');
-      findGameInstanceById();
-    }
-
-    function findGameInstanceById() {
-      const query = { _id: gameInstanceId };
-      const options = {};
-
-      db.collection('gameInstanceCollection').findOne(
-        query,
-        options,
-        (error, gameInstanceObject) => {
-          if (error) {
-            debug('findGameInstanceById: error:', error);
-            res
-              .status(503)
-              .send('503 Service Unavailable - Error', error.message);
-            return;
-          }
-
-          if (!gameInstanceObject) {
-            res
-              .status(503)
-              .send('503 Service Unavailable - Cannot find gameInstanceId');
-            return;
-          }
-
-          debug('findGameInstanceById', gameInstanceObject._id);
-          isPlayerTokenInGameInstanceObject(gameInstanceObject.playerArray);
-        }
-      );
-    }
-
-    function isPlayerTokenInGameInstanceObject(playerArray) {
-      let isFound = false;
-      playerArray.forEach((player) => {
-        if (player.token === playerToken) {
-          isFound = true;
-        }
-      });
-
-      if (!isFound) {
-        res
-          .status(503)
-          .send(
-            '503 Service Unavailable - Cannot find playerToken in gameInstance'
-          );
-        return;
-      }
-
-      debug('isPlayerTokenInGameInstanceObject:', isFound);
-      findMapLayer();
-    }
-
     function findMapLayer() {
+      const gameInstanceId = req.query.gameInstanceId;
       const query = { _id: gameInstanceId };
       const options = {};
 
