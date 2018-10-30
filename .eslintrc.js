2c4ce9f10f58f17880e76ad975ba3663e34f132b
Matteus <post@kalis.no>
2018-10-30 16:51:49 +0100

    Update .eslintrc.js to allow console.log

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,8 @@ module.exports = {
     'prefer-destructuring': 0, // Will not throw errors on syntax like: var something = request.body.something;
     'no-underscore-dangle': ['error', { allow: ['_id'] }], // Allow dangle for _id mongodb property
     'object-shorthand': ['error', 'consistent'], // Can use { variable: variable } in objects
-    'prefer-template': 0,
-    'arrow-body-style': ['error', 'always'] // Always braces when using arrow functions
+    'prefer-template': 0, // Disable so it is not always needed to use `template strings`
+    'arrow-body-style': ['error', 'always'], // Always braces when using arrow functions
+    'no-console': 0 // Allow console.log and not show any warnings
   }
 };
