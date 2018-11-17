400b8680fc721e5e9978292eddb4bc80d4268725
Matteus <post@kalis.no>
2018-11-17 16:32:44 +0100

    Clean main.js

--- client/main.js
+++ client/main.js
@@ -2,7 +2,6 @@
 
 'use strict';
 
-/* start.js */
 g.main = function main() {
   // authentication
   const auth = {};
@@ -15,14 +14,5 @@ g.main = function main() {
 
   g.launch.launch();
 
-  // copy walkie module from old phaser project
-  // add pixi module that initializes pixi canvas so it is ready when state changes to display world map etc.
-  // add UI modules that display and work on various html input elements without pixi
-
-  // add library that checks data from /ajax/readGameStateData every 0,5s
-  // that library annonces the state through walkie
-  // a module that should handle setup state displays relevant html and fills it with data from walkie
-
   // g.module.pixiTest();
 };
-/* /start.js */
