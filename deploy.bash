b49029691f737677f9049c971ad645a4240b0136
Matteus <post@kalis.no>
2018-12-07 17:44:36 +0100

    Create deploy.bash used to fast deploy through remote git pull on the remote server

---
+++ deploy.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "*** START deploy.bash ***"
+
+ssh ftpuser@51.68.142.93 -p 2222 << EOF
+echo " "
+echo "*** Change to cogs directory ***"
+cd ~/node/cogs/axe/current/
+pwd
+echo " "
+echo "*** Git pull ***"
+echo " "
+git pull
+echo " "
+echo "*** Build Client Javascript ***"
+echo " "
+./build_client.bash
+echo " "
+echo "*** Restart Node server ***"
+echo " "
+/home/ftpuser/npm/bin/pm2 restart cogs_axe
+EOF
+
+echo "*** END deploy.bash ***"
