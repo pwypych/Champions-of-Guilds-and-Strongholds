7686b53d22368b81c1fe619cfd7f3d9a94eae9f9
Matteus <post@kalis.no>
2018-12-29 14:17:58 +0100

    Fix launchCountdown bug, move it to end of middleware chain

--- server/app.js
+++ server/app.js
@@ -176,8 +176,8 @@ function setupLibrariesAndRoutes(figureManagerTree) {
     require('./ajax/launchState/ready/everyPlayerReadyChecker.js')(),
     require('./ajax/launchState/ready/preparePlayerResource.js')(db),
     require('./ajax/launchState/ready/prepareHeroFigure.js')(db),
-    require('./ajax/launchState/ready/unsetReadyForLaunch.js')(db),
-    require('./ajax/launchState/ready/launchCountdown.js')(db)
+    require('./ajax/launchState/ready/launchCountdown.js')(db),
+    require('./ajax/launchState/ready/unsetReadyForLaunch.js')(db)
   );
 
   app.post(
