f54cb0698ce075c628d258f80e9680a6f78d2236
Matteus <post@kalis.no>
2018-12-29 16:56:25 +0100

    F: Add resources .png, and use them in informationModal

---
+++ client/world/ui/informationModal.js
@@ -0,0 +1,15 @@
+// @format
+
+'use strict';
+
+g.world.informationModal = ($body) => {
+  const $modal = $body.find('#js-information-modal');
+
+  (function init() {
+    // onClick();
+  })();
+
+  function onClick() {
+    // gcc
+  }
+};
