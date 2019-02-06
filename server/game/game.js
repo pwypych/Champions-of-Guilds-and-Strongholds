82234874be655506653064c32b2356179228eea9
Matteus <post@kalis.no>
2019-02-06 17:07:37 +0100

    Add interfaceBattle.ejs

--- server/game/game.js
+++ server/game/game.js
@@ -31,6 +31,15 @@ module.exports = (environment, db, templateToHtml) => {
       templateToHtml(path, viewModel, (error, html) => {
         viewModel.htmlInterfaceWorld = html;
         debug('generateInterfaceWorld: html.length:', html.length);
+        generateInterfaceBattle();
+      });
+    }
+
+    function generateInterfaceBattle() {
+      const path = environment.basepath + '/server/game/interfaceBattle.ejs';
+      templateToHtml(path, viewModel, (error, html) => {
+        viewModel.htmlInterfaceBattle = html;
+        debug('generateInterfaceBattle: html.length:', html.length);
         generateChat();
       });
     }
