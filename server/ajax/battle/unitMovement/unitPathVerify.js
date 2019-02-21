0b70d07b6c6dce6f9c3ba00f329dbee9ebef5dad
Matteus <post@kalis.no>
2019-02-21 17:31:11 +0100

    Add verifyUnitPathCollision() to unitPathVerify.js

--- server/ajax/battle/unitMovement/unitPathVerify.js
+++ server/ajax/battle/unitMovement/unitPathVerify.js
@@ -4,6 +4,7 @@
 
 const debug = require('debug')('cogs:unitPathVerify');
 const validator = require('validator');
+const _ = require('lodash');
 
 module.exports = () => {
   return (req, res, next) => {
@@ -54,17 +55,53 @@ module.exports = () => {
       limitUnitPathByMovementPoints(entities, unitId, unitPath);
     }
 
-    function limitUnitPathByMovementPoints(entities, unitId, unitPath) {
+    function limitUnitPathByMovementPoints(entities, unitId, unitPathOriginal) {
       const unit = entities[unitId];
-      const stepAmount = unitPath.length - 1;
+      const stepAmount = unitPathOriginal.length - 1;
       const unitMovement = unit.unitStats.current.movement;
 
+      let unitPath;
+
       if (unitMovement < stepAmount) {
-        const unitPathLimited = unitPath.slice(0, unitMovement + 1);
-        res.locals.unitPath = unitPathLimited;
+        unitPath = unitPathOriginal.slice(0, unitMovement + 1);
+        res.locals.unitPath = unitPath;
+      } else {
+        unitPath = unitPathOriginal;
       }
 
       debug('limitUnitPathByMovementPoints', unitMovement, stepAmount);
+      verifyUnitPathCollision(entities, unitId, unitPath);
+    }
+
+    function verifyUnitPathCollision(entities, unitId, unitPathOriginal) {
+      let isCollision = false;
+      _.forEach(unitPathOriginal, (position, index) => {
+        if (index === 0) {
+          return;
+        }
+
+        _.forEach(entities, (entity) => {
+          if (entity.unitName && entity.collision && entity.position) {
+            if (
+              entity.position.x === position.x &&
+              entity.position.y === position.y
+            ) {
+              isCollision = index;
+            }
+          }
+        });
+      });
+
+      if (isCollision) {
+        debug('verifyUnitPathCollision: Collistion on path!');
+        res.send({ error: 1, message: 'Collistion on chosen path' });
+        return;
+      }
+
+      if (!isCollision) {
+        debug('verifyUnitPathCollision: No collistions!');
+      }
+
       next();
     }
   };
