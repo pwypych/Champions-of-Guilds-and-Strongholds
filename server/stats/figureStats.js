b19c07e216bcba87987ad488288330c6b8d48d94
Piotrek <ludzik3217@gmail.com>
2019-03-06 16:56:38 +0100

    Create figureStats.js module

---
+++ server/stats/figureStats.js
@@ -0,0 +1,12 @@
+// @format
+
+'use strict';
+
+const deepFreeze = require('deep-freeze');
+
+// What does this module do?
+// Returns object with all figures stats
+const unitStats = {};
+
+deepFreeze(unitStats);
+module.exports = unitStats;
