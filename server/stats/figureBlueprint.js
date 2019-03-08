ac3b9211cb34992c10c02509b6ee40972ffebefb
Piotrek <ludzik3217@gmail.com>
2019-03-08 23:28:29 +0100

    Add human, undead, beast units to figureBlueprint.js

--- server/stats/figureBlueprint.js
+++ server/stats/figureBlueprint.js
@@ -7,34 +7,130 @@
 
 module.exports = () => {
   return {
+    skeleton: {
+      unitAmounts: {
+        skeleton: 10
+      }
+    },
+
+    undeadRogue: {
+      unitAmounts: {
+        undeadRogue: 8
+      }
+    },
+
+    crazyWizard: {
+      unitAmounts: {
+        crazyWizard: 6
+      }
+    },
+
+    orc: {
+      unitAmounts: {
+        orc: 4
+      }
+    },
+
+    blackKnight: {
+      unitAmounts: {
+        blackKnight: 2
+      }
+    },
+
+    minotaur: {
+      unitAmounts: {
+        minotaur: 2
+      }
+    },
+
+    worm: {
+      unitAmounts: {
+        worm: 4
+      }
+    },
+
+    plant: {
+      unitAmounts: {
+        plant: 6
+      }
+    },
+
+    eyball: {
+      unitAmounts: {
+        eyball: 8
+      }
+    },
+
+    butterfly: {
+      unitAmounts: {
+        butterfly: 10
+      }
+    },
+
+    rogue: {
+      unitAmounts: {
+        rogue: 10
+      }
+    },
+
+    warrior: {
+      unitAmounts: {
+        warrior: 8
+      }
+    },
+
+    ranger: {
+      unitAmounts: {
+        ranger: 6
+      }
+    },
+
+    wizard: {
+      unitAmounts: {
+        wizard: 4
+      }
+    },
+
+    cleric: {
+      unitAmounts: {
+        cleric: 2
+      }
+    },
+
     alchemist: {
       unitAmounts: {
         alchemist: 5
       }
     },
+
     archer: {
       unitAmounts: {
         archer: 5
       }
     },
+
     bear: {
       unitAmounts: {
         bear: 5
       }
     },
+
     castleRandom: {
       spriteOffset: { x: -32, y: -64 },
       collision: true
     },
+
     crystal: {
       resource: { name: 'crystal', amount: 3 }
     },
+
     dirt: { collision: true },
     goblin: {
       unitAmounts: {
         goblin: 5
       }
     },
+
     gold: { resource: { name: 'gold', amount: 1500 } },
     rock: { collision: true },
     rogue: {
@@ -42,16 +138,19 @@ module.exports = () => {
         rogue: 5
       }
     },
+
     spearbarer: {
       unitAmounts: {
         spearbarer: 5
       }
     },
+
     stone: { resource: { name: 'stone', amount: 7 } },
     tree: {
       spriteOffset: { x: 0, y: -10 },
       collision: true
     },
+
     wood: { resource: { name: 'wood', amount: 5 } }
   };
 };
