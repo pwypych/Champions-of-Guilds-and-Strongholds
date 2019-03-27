aec0f6e2ba905f6e7d71cbd1868fb32c10bdceca
Piotrek <ludzik3217@gmail.com>
2019-03-27 18:07:38 +0100

    F: Generate dynamicly select race options in new module launchSelectRaceOptions.js

---
+++ client/launch/launchSelectRaceOptions.js
@@ -0,0 +1,43 @@
+// @format
+
+'use strict';
+
+g.launch.launchSelectRaceOptions = (
+  $body,
+  walkie,
+  freshEntities,
+  blueprints
+) => {
+  const $select = $body.find('#js-launch .js-select-race');
+
+  (function init() {
+    onEntitiesGet();
+  })();
+
+  function onEntitiesGet() {
+    walkie.onEvent(
+      'entitiesGetFirst_',
+      'launchSelectRaceOptions.js',
+      () => {
+        const gameEntity = freshEntities()[freshEntities()._id];
+
+        if (gameEntity.state !== 'launchState') {
+          return;
+        }
+
+        updateSelect();
+      },
+      false
+    );
+  }
+
+  function updateSelect() {
+    const raceBlueprint = blueprints.raceBlueprint;
+    $select.empty();
+
+    _.forEach(raceBlueprint, (race, id) => {
+      const $option = $('<option value="' + id + '">' + id + '</option>');
+      $select.append($option);
+    });
+  }
+};
