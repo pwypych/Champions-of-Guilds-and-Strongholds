3a5dd5af8555e077a80f15b0ad7bc2dbfbdb6bfa
Piotrek <ludzik3217@gmail.com>
2019-03-28 17:30:04 +0100

    Add decideManeuverDecrementType() and updateSetUnitManeuverToZero() in decrementUnitManeuver.js

--- server/ajax/battle/maneuver/digest/decrementUnitManeuver.js
+++ server/ajax/battle/maneuver/digest/decrementUnitManeuver.js
@@ -11,8 +11,17 @@ module.exports = (db) => {
       const gameId = res.locals.entities._id;
       const entityId = res.locals.entityId;
 
-      updateUnitManeuverPoints(gameId, entityId);
+      decideManeuverDecrementType(gameId, entityId);
     })();
+
+    function decideManeuverDecrementType(gameId, entityId) {
+      if (res.locals.attack) {
+        updateSetToZeroUnitManeuver(gameId, entityId);
+        return;
+      }
+
+      updateUnitManeuverPoints(gameId, entityId);
+    }
 
     function updateUnitManeuverPoints(gameId, entityId) {
       const query = { _id: gameId };
@@ -22,6 +31,31 @@ module.exports = (db) => {
       $inc[field] = -1;
 
       const update = { $inc: $inc };
+      const options = {};
+
+      db.collection('gameCollection').updateOne(
+        query,
+        update,
+        options,
+        (error) => {
+          if (error) {
+            debug('updateUnitManeuverPoints: error: ', error);
+          }
+
+          debug('updateUnitManeuverPoints: Success!');
+          next();
+        }
+      );
+    }
+
+    function updateSetToZeroUnitManeuver(gameId, entityId) {
+      const query = { _id: gameId };
+
+      const field = entityId + '.unitStats.current.maneuverPoints';
+      const $set = {};
+      $set[field] = 0;
+
+      const update = { $set: $set };
       const options = {};
 
       db.collection('gameCollection').updateOne(
