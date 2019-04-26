1047b168140366071438decbc5ce82a4acc08351
Matteus <post@kalis.no>
2019-04-26 17:40:31 +0200

    F: Add unitEscapingIndicator.js and make it work

---
+++ client/battle/unitMovement/unitEscapingIndicator.js
@@ -0,0 +1,106 @@
+// @format
+
+'use strict';
+
+g.battle.unitEscapingIndicator = (walkie, viewport, freshEntities) => {
+  const blockWidthPx = 32;
+  const blockHeightPx = 32;
+
+  const battleContainer = viewport.getChildByName('battleContainer');
+
+  (function init() {
+    onRecentActivityDifferance();
+  })();
+
+  function onRecentActivityDifferance() {
+    walkie.onEvent(
+      'recentActivityDifferanceFound_',
+      'unitEscapingIndicator.js',
+      (data) => {
+        if (data.entity.recentActivity.name === 'onMovement') {
+          const entityId = data.entityId;
+          const path = data.entity.recentActivity.path;
+          const positionInitial = path[0];
+
+          forEachPositionAroundUnit(entityId, positionInitial);
+        }
+      },
+      false
+    );
+  }
+
+  function forEachPositionAroundUnit(entityId, positionInitial) {
+    const entities = freshEntities();
+    const unit = entities[entityId];
+
+    let positionEnemy;
+
+    [{ x: 0, y: -1 }, { x: 1, y: 0 }, { x: 0, y: 1 }, { x: -1, y: 0 }].forEach(
+      (offset) => {
+        const positionMaybeEnemy = {};
+        positionMaybeEnemy.x = positionInitial.x + offset.x;
+        positionMaybeEnemy.y = positionInitial.y + offset.y;
+        // console.log('forEachPositionAroundUnit:unitPosition:', positionInitial);
+        // console.log('forEachPositionAroundUnit:position:', position);
+        if (toolIsEnemyOnPosition(entities, unit, positionMaybeEnemy)) {
+          positionEnemy = positionMaybeEnemy;
+        }
+      }
+    );
+
+    if (positionEnemy) {
+      console.log(
+        // 'unitEscapingIndicator: Yes, enemy found nearby!',
+        positionEnemy
+      );
+      instantiateIndicator(positionInitial);
+    }
+
+    // console.log('unitEscapingIndicator: No enemy to escape!');
+  }
+
+  function toolIsEnemyOnPosition(entities, unit, positionMaybeEnemy) {
+    let isEnemy;
+
+    _.forEach(entities, (entity) => {
+      if (unit.boss !== entity.boss) {
+        if (entity.unitName && entity.unitStats && !entity.dead) {
+          if (
+            entity.position.x === positionMaybeEnemy.x &&
+            entity.position.y === positionMaybeEnemy.y
+          ) {
+            isEnemy = true;
+          }
+        }
+      }
+    });
+
+    return isEnemy;
+  }
+
+  function instantiateIndicator(position) {
+    const amountTextStyle = new PIXI.TextStyle({
+      fontFamily: 'Arial',
+      fontSize: 12,
+      fontWeight: 'bolder',
+      fill: 'white',
+      strokeThickness: 2
+    });
+
+    const amountText = new PIXI.Text('Retreat!', amountTextStyle);
+    amountText.name = 'hasBeenCollectedIndicator';
+    battleContainer.addChild(amountText);
+    amountText.x =
+      blockWidthPx * position.x + blockWidthPx / 2 - amountText.width / 2;
+    amountText.y =
+      blockHeightPx * position.y + blockHeightPx / 2 - amountText.height / 2;
+
+    destroyAfterTimeout(amountText);
+  }
+
+  function destroyAfterTimeout(amountText) {
+    setTimeout(() => {
+      amountText.destroy();
+    }, 500);
+  }
+};
