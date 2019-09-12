fb9467604f6120815188cbd1e8d7b2f03c315ba6
Piotrek <ludzik3217@gmail.com>
2019-09-12 18:14:42 +0200

    Add abstract prefix to all figures files that are abstract

--- server/panel/generateMap/generateFigureMap.js
+++ server/panel/generateMap/generateFigureMap.js
@@ -91,12 +91,12 @@ module.exports = (environment, unitBlueprint) => {
         const figureChance = _.random(0, 99);
         const monsterIndex = _.random(0, monsterTierArray[tier].length - 1);
 
-        if (abstractFigure.figureName === 'monster') {
+        if (abstractFigure.figureName === 'abstractMonster') {
           figureMap[y][x] = monsterTierArray[tier][monsterIndex].name;
           return;
         }
 
-        if (abstractFigure.figureName === 'monsterMaybe') {
+        if (abstractFigure.figureName === 'abstractMonsterMaybe') {
           figureMap[y][x] = 'empty';
 
           if (figureChance > 60) {
@@ -117,12 +117,12 @@ module.exports = (environment, unitBlueprint) => {
 
         const figureChance = _.random(0, 99);
 
-        if (abstractFigure.figureName === 'barrier') {
+        if (abstractFigure.figureName === 'abstractBarrier') {
           figureMap[y][x] = barrierArray[_.random(0, barrierArray.length - 1)];
           return;
         }
 
-        if (abstractFigure.figureName === 'barrierMaybe') {
+        if (abstractFigure.figureName === 'abstractBarrierMaybe') {
           figureMap[y][x] = 'empty';
 
           if (figureChance > 40) {
@@ -144,13 +144,13 @@ module.exports = (environment, unitBlueprint) => {
 
         const figureChance = _.random(0, 99);
 
-        if (abstractFigure.figureName === 'treasure') {
+        if (abstractFigure.figureName === 'abstractTreasure') {
           figureMap[y][x] =
             treasureArray[_.random(0, treasureArray.length - 1)];
           return;
         }
 
-        if (abstractFigure.figureName === 'treasureMaybe') {
+        if (abstractFigure.figureName === 'abstractTreasureMaybe') {
           figureMap[y][x] = 'empty';
 
           if (figureChance > 40) {
