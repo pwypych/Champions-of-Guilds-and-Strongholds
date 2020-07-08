10122dcadb34f31f0ca70a4d10e1dc45a582c4f3
Piotrek <ludzik3217@gmail.com>
2020-07-08 23:26:24 +0200

    Rename hero__ to hero__figure__

--- server/ajax/launch/ready/prepareHeroFigure.js
+++ server/ajax/launch/ready/prepareHeroFigure.js
@@ -81,7 +81,7 @@ module.exports = (db, raceBlueprint) => {
       const gameId = entities._id;
       const query = { _id: gameId };
 
-      const field = 'hero__' + shortId.generate();
+      const field = 'hero__figure__' + shortId.generate();
       const $set = {};
       $set[field] = hero;
       const update = { $set: $set };
