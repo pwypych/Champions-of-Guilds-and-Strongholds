8caa52ceeb323554c2551d480f5f3641d21b6b5d
Piotrek <ludzik3217@gmail.com>
2020-07-18 01:32:37 +0200

    Add blueprintMonster.hook.js files for plant, ranger, rogue, skeleton, undeadRogue, wizzard, warrior and worm

---
+++ server/plugin/unit/wizard/blueprintMonsterWizard.hook.js
@@ -0,0 +1,16 @@
+// @format
+
+'use strict';
+
+// What does this module do?
+// It registeres NPC monster for map generation and random figure assignment
+module.exports = (hook) => {
+  hook.attach('registerMonster_', (injected, done) => {
+    injected.monsterArray.push({
+      name: 'wizard',
+      tier: 4
+    });
+
+    done();
+  });
+};
