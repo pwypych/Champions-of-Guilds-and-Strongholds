6958e47f288cd430e1e2462f0deef7188d323a3f
Piotrek <ludzik3217@gmail.com>
2020-10-17 23:15:39 +0200

    Create module c.prohibitBuilded.js

---
+++ server/game/world/fortificationBuild/c.prohibitBuilded.js
@@ -0,0 +1,74 @@
+// @format
+
+'use strict';
+
+// What does this module do
+// Disable buy buttons of fortification that cannot be build
+
+g.autoload.prohibitBuilded = (inject) => {
+  const $body = inject.$body;
+  const walkie = inject.walkie;
+  const freshEntities = inject.freshEntities;
+
+  const $modal = $body.find('[data-world-interface-fortification-modal]');
+
+  (function init() {
+    onFortificationModalDrawn();
+  })();
+
+  function onFortificationModalDrawn() {
+    walkie.onEvent(
+      // 'fortificationModalDrawn',
+      'viewportWorldReady_',
+      'prohibitBuilded.js',
+      () => {
+        const gameEntity = freshEntities()[freshEntities()._id];
+
+        if (gameEntity.state !== 'worldState') {
+          return;
+        }
+
+        findPlayerId();
+      },
+      false
+    );
+  }
+
+  function findPlayerId() {
+    let playerId;
+    _.forEach(freshEntities(), (entity, id) => {
+      if (entity.playerCurrent) {
+        playerId = id;
+      }
+    });
+
+    generateFortificationBuildedArray(playerId);
+  }
+
+  function generateFortificationBuildedArray(playerId) {
+    const fortificationBuildedArray = [];
+    _.forEach(freshEntities(), (entity) => {
+      if (entity.fortificationName && entity.owner === playerId) {
+        fortificationBuildedArray.push(entity.fortificationName);
+      }
+    });
+
+    disableButton(fortificationBuildedArray);
+  }
+
+  function disableButton(fortificationBuildedArray) {
+    const buildButtonsArray = $modal.find('button').toArray();
+
+    buildButtonsArray.forEach(($button) => {
+      const fortificationName = $($button).data('fortification-name');
+      const isFortificationBuilded = _.includes(
+        fortificationBuildedArray,
+        fortificationName
+      );
+
+      if (isFortificationBuilded) {
+        $($button).attr('disabled', 'disabled');
+      }
+    });
+  }
+};
