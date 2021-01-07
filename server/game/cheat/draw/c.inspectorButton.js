bbcf82746d533562d6bbab37f39f23596efc9653
Piotrek <ludzik3217@gmail.com>
2021-01-07 21:13:57 +0100

    Rewrite all js-inspector classes to data attribute

--- server/game/cheat/draw/c.inspectorButton.js
+++ server/game/cheat/draw/c.inspectorButton.js
@@ -8,7 +8,7 @@ g.autoload.inspectorButton = (inject) => {
   const $body = inject.$body;
   const auth = inject.auth;
 
-  const $link = $body.find('.js-inspector');
+  const $link = $body.find('[data-inspector]');
 
   (function init() {
     $link.attr('href', '/inspector' + auth.uri);
