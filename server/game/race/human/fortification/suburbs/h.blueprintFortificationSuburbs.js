d0635f3aa0f92d9a50a1a49ab38d348410c89478
Piotrek <ludzik3217@gmail.com>
2021-08-05 23:39:04 +0200

    [Partial] Add fortification suburbs for human rogue unit

---
+++ server/game/race/human/fortification/suburbs/h.blueprintFortificationSuburbs.js
@@ -0,0 +1,21 @@
+// @format
+
+'use strict';
+
+// What does this module do?
+// It attaches blueprint for this fortification
+module.exports = (hook) => {
+  hook.attach('generateBlueprints_', (injected, done) => {
+    injected.blueprint.fortification.suburbs = {
+      fortificationName: 'suburbs',
+      namePretty: 'Town suburbs',
+      race: 'human',
+      cost: {
+        gold: 3000,
+        stone: 5
+      }
+    };
+
+    done();
+  });
+};
